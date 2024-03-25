3057ca14c18d4dbc615e285d90da36248321e1c1
Majd Zeino <majd.maher10@gmail.com>
2024-03-25 23:51:41 +0400

    removal of Backend file

--- Backend/ChatBox/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('chat/', ExternalServiceView.as_view(), name='external_service'),
-    path('chat/history/', UserChatHistoryView.as_view(), name='chat_history'),
-]
